eee45808154591b4e252acb0953162ddfbc0adc3
Sarah-Rafaella <seu.email@example.com>
2026-03-05 14:40:23 -0300

    Initial commit

---
+++ configurações.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(f"OpenCV versão: {cv2.__version__}")
+print(f"NumPy versão: {np.__version__}")
+ 
+
+import pytesseract
+print(f"Tesseract: {pytesseract.get_tesseract_version()}")
